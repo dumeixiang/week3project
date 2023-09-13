b6d35b76137cc3a1067499eec340189f396ff5c7
dumeixiang <141801043+dumeixiang@users.noreply.github.com>
2023-09-12 20:40:39 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 """
 Main code
 """
+def development(data): 
+    return data["Adolescent fertility rate (births per 1,000 women ages 15-19)"].describe().loc['mean']
 
+import seaborn.objects as so
+from matplotlib import style
 
-def negative_number(a):
-    if a < 0:
-        return True
+def plot(data):
+    my_chart = (so.Plot(data, x="Mortality rate, infant (per 1,000 live births)", y="GDP per capita (constant 2010 US$)")
+    .add(so.Line(), so.PolyFit(order=2))
+    .add(so.Dot())
+    .label(title="Log GDP and infat Mortality")
+    .theme({**style.library["seaborn-whitegrid"]}))
+    my_chart.show()
+
 
